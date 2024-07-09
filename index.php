fd10da1ffb9863b2b535708baeb48731b7fec801
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-07-09 21:43:54 +0300

    .

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php echo 'Hello World!' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Demo</title>
+</head>
+<body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+    $message = "";
+
+    if ($read) {
+        $message = "You have read \"<?php echo $name; ?>\"";
+    }
+    else {
+        $message = 'You not have read "<?php echo $name; ?>"';
+    }
+    ?>
+
+    <h1>
+        
+    </h1>
+</body>
+</html>
