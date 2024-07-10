84e0db2e9db23761ceb1bdee94252eef65c3c7f4
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-07-10 05:43:24 +0300

    Associative arrays

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Demo</title>
 </head>
+
 <body>
     <?php
     $name = "Dark Matter";
@@ -14,8 +16,7 @@
 
     if ($read) {
         $message = "You have read \"$name\"";
-    }
-    else {
+    } else {
         $message = "You have not read \"$name\"";
     }
 
@@ -24,23 +25,22 @@
 
     <h1>
         <?= $message ?>
-    </h1>   
+    </h1>
 
     <ol>
-    <?php 
-    for ($i=0; $i < count($books); $i++) { 
+        <?php
+        for ($i = 0; $i < count($books); $i++) {
         ?> <li><?= $books[$i] ?></li><?php
-        // echo "<li>$books[$i]</li>";
-    }
-    ?>
+                                        // echo "<li>$books[$i]</li>";
+                                    }
+                                        ?>
     </ol>
 
     <ul>
-        <?php foreach ($books as $book) 
-        {
-            ?> <li><?= $book ?></li><?php
-        }
-        ?>
+        <?php foreach ($books as $book) {
+        ?> <li><?= $book ?></li><?php
+                                }
+                                    ?>
     </ul>
 
     <ol>
@@ -48,6 +48,43 @@
             <li><?= $book ?></li>
         <?php endforeach ?>
     </ol>
-    
+
+    <?php
+
+    $books2 = [
+        [
+            'name' => '1984',
+            'author' => 'Oruel',
+            'year' => '1789'
+        ],
+
+        [
+            'name' => '451F',
+            'author' => 'Bredbery',
+            'year' => '1764'
+        ],
+
+        [
+            'name' => 'Bibli',
+            'author' => 'God',
+            'year' => '1943'
+        ]
+    ];
+
+    ?>
+
+    <ul>
+        <?php foreach ($books2 as $book2) : ?>
+            <li><?= $book2['author'] ?></li>
+        <?php endforeach ?>
+    </ul>
+
+    <ol>
+        <?php foreach ($books2 as $book2) : ?>
+            <li><?= $book2['name'] . ' year: ' . $book2['year'] ?></li>
+            <?php endforeach ?>
+    </ol>
+
 </body>
+
 </html>
